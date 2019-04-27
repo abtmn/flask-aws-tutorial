5403047e7e3788eaa8a760e1d6843bca298cb745
Андрей <andrej@MacBook-Pro-Andrej.local>
2019-04-27 12:27:17 +0300

    123

--- application/models.py
+++ application/models.py
@@ -4,8 +4,35 @@ class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     notes = db.Column(db.String(128), index=True, unique=False)
     
+    
     def __init__(self, notes):
         self.notes = notes
 
     def __repr__(self):
         return '<Data %r>' % self.notes
+
+class Loaners(db.Model):
+    id_loaner = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True, unique=False)
+    surname = db.Column(db.String(128), index=True, unique=False)
+    
+    
+    def __init__(self, id_loaner):
+        self.id_loaner = id_loaner
+
+    def __repr__(self):
+        return '<Data %r>' % self.id_loaner
+
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Data %r>' % self.name
+        
+
+    def __init__(self, surname):
+        self.surname = surname
+
+    def __repr__(self):
+        return '<Data %r>' % self.surname
