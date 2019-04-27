82112f324046e4e6a706589dbf9781dc58b2bd07
Андрей <andrej@MacBook-Pro-Andrej.local>
2019-04-27 11:57:10 +0300

    123

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://loginDb:12345678@dbandrew.cnnosg0uu60u.us-east-2.rds.amazonaws.com/AndrewDB
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://loginDb:12345678@dbandrew.cnnosg0uu60u.us-east-2.rds.amazonaws.com/AndrewDB'
 '
 
 # Uncomment the line below if you want to work with a local DB
