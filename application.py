c53d366a92369fda9eb2f07515a88c73b3a1ffab
Андрей <andrej@MacBook-Pro-Andrej.local>
2019-04-27 12:14:29 +0300

    312

--- application.py
+++ application.py
@@ -37,7 +37,7 @@ def index():
         return render_template('thanks.html', notes=form1.dbNotes.data)
 
     if request.method == 'POST' and addLoanerName.validate():
-        data_entered = loaners(name=addLoanerName.dbNotes.loaners)
+        data_entered = Data(name=addLoanerName.dbNotes.loaners)
         try:     
             db.session.add(data_entered)
             db.session.commit()        
